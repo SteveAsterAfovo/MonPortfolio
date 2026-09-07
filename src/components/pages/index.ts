97516fe956d590fab22747ec8e4802e109942411
steveasterafovo <steveasterafovo@gmail.com>
2026-09-07 03:04:48 +0100

    s

---
+++ src/components/pages/index.ts
@@ -0,0 +1,6 @@
+export { About } from "./About";
+export { Contact } from "./Contact";
+export { Credentials } from "./Credentials";
+export { WhatsAppFab } from "./WhatsAppFab";
+export { WhatsAppFab } from "./WhatsAppFab";
+export { WhatsAppFab } from "./WhatsAppFab";
