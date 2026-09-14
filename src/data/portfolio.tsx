9e97121659e59918e7ccf83a88284fbbaa1e466a
steveasterafovo <steveasterafovo@gmail.com>
2026-09-14 23:41:59 +0100

    d

--- src/data/portfolio.tsx
+++
@@ -1,3 +0,0 @@
-import { portfolioData } from "@/data/content";
-
-export const { identity, profile, stack, stackIntro, stackFooter, projects, certifications, education, autodidact } = portfolioData;
