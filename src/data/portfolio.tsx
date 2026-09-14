4cfa1af8739d80e8654fdb6324cffa43822d8964
steveasterafovo <steveasterafovo@gmail.com>
2026-09-14 23:04:27 +0100

    s

--- src/data/portfolio.tsx
+++ src/data/portfolio.tsx
@@ -2,5 +2,4 @@ import { portfolioData } from "@/data/content";
 
 export const { identity, profile, stack, stackIntro, stackFooter, projects, certifications, education, autodidact } = portfolioData;
 
-export const STAFPRINT_URL = "https://stafprint.com/";
 export const WHATSAPP_URL = "https://wa.me/2290166523639";
