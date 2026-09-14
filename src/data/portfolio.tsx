963ca72a5710abfe5c2703a5f8ba2d363968bd66
steveasterafovo <steveasterafovo@gmail.com>
2026-09-14 23:06:26 +0100

    s

--- src/data/portfolio.tsx
+++ src/data/portfolio.tsx
@@ -1,5 +1,3 @@
 import { portfolioData } from "@/data/content";
 
 export const { identity, profile, stack, stackIntro, stackFooter, projects, certifications, education, autodidact } = portfolioData;
-
-export const WHATSAPP_URL = "https://wa.me/2290166523639";
