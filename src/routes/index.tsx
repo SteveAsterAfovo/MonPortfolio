06ddd5515eb2f1e8487ab28d23562c5a2fc724f6
steveasterafovo <steveasterafovo@gmail.com>
2026-09-15 00:14:59 +0100

    s

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { SiteShell } from "@/components/site";
 import { Hero, Projects, About, Stack, Credentials, Contact } from "@/components/pages";
-import { identity, projects, stack } from "@/data/content";
+import { identity, stack } from "@/data/content";
+import { projects } from "@/data";
 import { URL } from "@/data/content";
 
 const AUTHOR_NAME = "Steve Aster D. AFOVO";
