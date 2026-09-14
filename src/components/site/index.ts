cebfbae168351c83df9278135d0fae677084c72f
steveasterafovo <steveasterafovo@gmail.com>
2026-09-14 20:41:02 +0100

    s

--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -2,4 +2,4 @@ export { ThemeToggle } from "./ThemeToggle";
 export { Header } from "./Header";
 export { Footer } from "./Footer";
 export { WhatsAppFab } from "./WhatsAppFab";
-
+export { CookieConsent } from "./CookieConsent";
