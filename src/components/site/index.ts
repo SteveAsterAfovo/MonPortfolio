1f766b61fe0f6fd8e79565c1d806fac42cae316c
steveasterafovo <steveasterafovo@gmail.com>
2026-09-14 22:21:34 +0100

    s

--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -1,6 +1,7 @@
 export { ThemeToggle } from "./ThemeToggle";
 export { Header } from "./Header";
 export { Footer } from "./Footer";
+export { SiteShell } from "./SiteShell";
 export { WhatsAppFab } from "./WhatsAppFab";
 export { CookieConsent } from "./CookieConsent";
 export { SteveMobLogo, SteveDeskLogo } from "./SteveLogo";
