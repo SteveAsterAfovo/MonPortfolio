e3ac2e8e2663e31816da42708057eeb712e08611
steveasterafovo <steveasterafovo@gmail.com>
2026-09-15 12:40:40 +0100

    s

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -93,8 +93,6 @@ export const projects: Project[] = [
     ],
     stack: ["Nuxt.js", "Sails.js", "Node.js", "Tailwind", "PostgreSQL"],
     url: "https://hire-suitops.netlify.app/",
-    logo: `${PGS_LOGO_BASE}/Hire-MC.png`,
-    logoDesk: `${PGS_LOGO_BASE}/Hire-DC.png`,
   },
   {
     id: "eqt",
