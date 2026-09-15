c0e9397723b22f709dd8c67734a3d0a21945c636
steveasterafovo <steveasterafovo@gmail.com>
2026-09-15 08:09:54 +0100

    s

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,3 +1,4 @@
 export { identity, profile } from "./me";
 export { stacks, type StackCategory } from "./stacks";
 export { projects, type Project } from "./projects";
+export { projects, type Project } from "./projects";
