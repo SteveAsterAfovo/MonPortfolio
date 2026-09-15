d4721a0b914ee0d0d9d5c536d7f97c2e1f0e375f
steveasterafovo <steveasterafovo@gmail.com>
2026-09-15 10:17:37 +0100

    w

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,7 +3,7 @@ import { Outlet, createRootRouteWithContext, HeadContent, Scripts } from "@tanst
 import { type ReactNode } from "react";
 import appCss from "../styles.css?url";
 import { NotFoundComponent, ErrorComponent } from "@/components/errors";
-import { LINKS } from "@/data/content";
+import { LINKS } from "@/data";
 import logo from "@/assets/logos.json";
 
 // Constantes pour éviter les répétitions
